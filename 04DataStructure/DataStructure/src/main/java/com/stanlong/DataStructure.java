3794a5a1e7c17567ac4628aad144725963a7845d
StanLong <youlinbhz@sina.cn>
2021-12-08 22:22:58 +0800

    环形队列

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -1,10 +1,12 @@
 package com.stanlong;
 
+import java.util.Scanner;
+
 /**
  * 环形队列
  * 特点：类似手表上的表盘。
  *
- * 环形数组模拟队列思路：
+ * 数组模拟环形队列思路：
  * 以手表的表盘为例， [0，1，2，3，4，5，6，7，8，9，10，11]
  *
  * 留出一个空闲位表示队满和队空
@@ -34,8 +36,128 @@ package com.stanlong;
 public class DataStructure {
 
     public static void main(String[] args) throws Exception {
+        CircleArray ca = new CircleArray(4);
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        char key = ' ';
+        while (loop){
+            System.out.println("s(show)显示队列");
+            System.out.println("a(add)添加数据到队列");
+            System.out.println("h(head)查看队首元素");
+            System.out.println("g(get)取出队首元素");
+            System.out.println("e(exit)退出");
+            System.out.print("输入选择: ");
+            key = scanner.next().charAt(0);
 
+            switch (key){
+                case 's':
+                    try {
+                        ca.showQueue();
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'a':
+                    try{
+                        System.out.print("输入入队元素: ");
+                        int data = scanner.nextInt();
+                        ca.addQueue(data);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try{
+                        int result = ca.headQueue();
+                        System.out.println("队首元素: " + result);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'g':
+                    try{
+                        int result = ca.getQueue();
+                        System.out.println("出队元素: " + result);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
+}
 
+// 第二大类代码实现
+class CircleArray{
+    private int maxSize;
+    private int front;
+    private int rear;
+    private int[] arr;
+
+    public CircleArray(int maxSize){
+        this.maxSize = maxSize;
+        front = 0;
+        rear = 0;
+        arr = new int[maxSize];
+    }
+
+    // 判断队列是否为空
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    // 判断队列是否已满
+    public boolean isFull(){
+        return (rear + 1) % maxSize == front;
+    }
+
+    // 返回对首元素
+    public int headQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列是空的， 没有头元素");
+        }
+        return arr[front];
+    }
+
+    // 新增队列元素
+    public void addQueue(int data){
+        if(isFull()){
+            throw new RuntimeException("队列满了，无法添加元素");
+        }
+        arr[rear] = data;
+        rear = (rear + 1) % maxSize; // 取模是为了避免数组越界
+    }
+
+    // 元素出队列
+    public int getQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列是空的，没有可出队列的元素");
+        }
+
+        // 1. 先把front指向的值保存到一个临时变量
+        // 2. 将front后移，取模
+        // 3. 返回临时变量
+        int value = arr[front];
+        front = (front+1) % maxSize;
+        return value;
+    }
+
+    // 遍历队列中的所有元素
+    public void showQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列是空的");
+        }
+
+        for(int i = front; i< front + ((rear-front+maxSize)%maxSize); i++){
+            System.out.printf("arr[%d]=%d\n", i%maxSize, arr[i%maxSize]);
+        }
+
+    }
 }
