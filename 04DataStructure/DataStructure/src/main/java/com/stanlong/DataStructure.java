79188bee06b17cc46109939f19f08d672f613f2d
StanLong <youlinbhz@sina.cn>
2021-12-10 23:47:52 +0800

    链表

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -13,15 +13,21 @@ public class DataStructure {
 
     public static void main(String[] args) throws Exception {
         SingleLinkedList sll = new SingleLinkedList();
-        Node node01 = new Node(1);
-        Node node02 = new Node(2);
-        Node node03 = new Node(3);
-        Node node04 = new Node(4);
+        Node node01 = new Node(4);
+        Node node02 = new Node(5);
+        Node node03 = new Node(1);
+        Node node04 = new Node(2);
 
-        sll.add(node01);
-        sll.add(node02);
-        sll.add(node03);
-        sll.add(node04);
+        // sll.add(node01);
+        // sll.add(node02);
+        // sll.add(node03);
+        // sll.add(node04);
+
+        sll.insert(node01);
+        sll.insert(node02);
+        sll.insert(node03);
+        sll.insert(node04);
+
 
         sll.show();
     }
@@ -47,7 +53,7 @@ class SingleLinkedList{
     // 初始化头节点
     private Node headNode = new Node(0); // 头节点不存放具体的数据
 
-    // 添加
+    // 添加(在尾部追加)
     public void add(Node node){
         // head 节点不能动，因此先定义一个辅助节点帮助遍历
         Node temp = headNode;
@@ -61,6 +67,33 @@ class SingleLinkedList{
             temp = temp.next;
         }
         // 2. 将最后节点的指针域 next 指向新节点
+        temp.next = node;
+    }
+
+    // 插入
+    public void insert(Node node){
+        Node temp = headNode;
+        // 遍历单链表，查询插入位置
+        while (true){
+            if(temp.next == null){ // 如果链表是空的，退出循环
+                break;
+            }
+
+            if(temp.next.data > temp.data){ // 找到可以插入的位置，退出循环
+                break;
+            }
+            // 以节点数据与 1 2 3 4 为例
+            // temp.next.data > temp.data
+            // 进入链表的顺序为 head, head->1, head->2->1, head->3->2->1, head->4->3->2->1
+            // 即最先插入的排在链表末尾，最后插入的排在链表头部， 遍历的时候，后进先出(栈)
+
+            // temp.next.data < temp.data
+            // 进入链表的顺序为 head, head->1, head->1->2, head->1->2->3,head->1->2->3->4,
+            // 即最先插入的排在链表头部，最后插入的排在链表末尾， 遍历的时候，先入先出（队列）
+
+            temp = temp.next; // 后移，遍历当前列表
+        }
+        node.next = temp.next;
         temp.next = node;
     }
 
@@ -85,5 +118,4 @@ class SingleLinkedList{
 
     // 修改
     // 删除
-
 }
