b9b3e6ee005533f0d74e3998677274a4bd605936
StanLong <youlinbhz@sina.cn>
2022-02-08 22:33:01 +0800

    线索二叉树

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -1,104 +1,101 @@
 package com.stanlong;
 
-import lombok.*;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
- * 删除二叉树节点
+ * 顺序存储二叉树
  */
 public class DataStructure {
 
     public static void main(String[] args) throws Exception {
-        // 创建一颗二叉树
-        BinaryTree bt = new BinaryTree();
+        int array[] = {1,2,3,4,5,6,7};
+        ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(array);
+        System.out.println("--------------前序遍历顺序存储二叉树--------------");
+        arrayBinaryTree.preOrder();
+        System.out.println("--------------中序遍历顺序存储二叉树--------------");
+        arrayBinaryTree.infixOrder();
+        System.out.println("--------------后序遍历顺序存储二叉树--------------");
+        arrayBinaryTree.postOrder();
 
-        // 创建需要的节点
-        Node root = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-        Node node5 = new Node(5);
-        Node node6 = new Node(6);
-        Node node7 = new Node(7);
-
-        // 把节点挂到树上
-        bt.setRoot(root); // 设置树的根节点
-        root.setLeft(node2);
-        root.setRight(node3);
-        node2.setLeft(node4);
-        node2.setRight(node5);
-        node3.setLeft(node6);
-        node3.setRight(node7);
-
-        bt.preOrder(); // 前序遍历
-        System.out.println("--------------删除分割线--------------");
-        bt.delNode(100); // 删除节点7
-        bt.preOrder(); // 前序遍历
     }
 }
 
-@Getter
-@Setter
-@ToString(exclude = {"left", "right"})
-@RequiredArgsConstructor
-class Node{
-    @NonNull
-    private int id; // 数据域
-    private Node left; // 左指针
-    private Node right;  // 右指针
-
-    public void preOrder(){
-        System.out.println(this);
-        if(this.left != null){
-            this.left.preOrder();
-        }
-        if(this.right != null){
-            this.right.preOrder();
-        }
-    }
-
-    // 删除节点
-    public void delNode(int id){
-        if(this.left != null && this.left.getId() == id){
-            this.left = null;
-            return;
-        }
-        if(this.right != null && this.right.getId() == id){
-            this.right = null;
-            return;
-        }
-        if(this.left != null){
-            this.left.delNode(id);
-        }
-        if(this.right != null){
-            this.right.delNode(id);
-        }
-    }
-}
 
 // 创建一个二叉树
-class BinaryTree{
-    @Setter
-    private Node root; // 声明二叉树的根节点
+@RequiredArgsConstructor
+class ArrayBinaryTree{
+    @NonNull
+    private int[] array; // 有参构造方法
 
-    // 前序遍历
+    // 重载 preOrder， 在主方法中调用时就不需要传参了
     public void preOrder(){
-        if(this.root != null){
-            this.root.preOrder();
-        }else {
-            System.out.println("空树，没有节点");
+        this.preOrder(0);
+    }
+
+    public void infixOrder(){
+        this.infixOrder(0);
+    }
+
+    public void postOrder(){
+        this.postOrder(0);
+    }
+
+
+
+    // 前序遍历二叉树顺序存储结构
+    public void preOrder(int index){
+        if(array == null || array.length == 0){
+            System.out.println("数组是空的");
+            return;
+        }
+        // 打印当前节点
+        System.out.println(array[index]);
+
+        // 迭代左边
+        if(index*2 + 1 < array.length){
+            preOrder(index*2 + 1);
+        }
+
+        // 迭代右边
+        if(index*2+2 < array.length){
+            preOrder(index*2+2);
         }
     }
 
-    // 删除节点
-    public void delNode(int id){
-        if(root != null){
-            if(root.getId() == id){
-                root = null;
-            }else {
-                root.delNode(id);
-            }
-        }else{
-            System.out.println("空树，不能删除");
+    // 中序遍历二叉树顺序存储结构
+    public void infixOrder(int index){
+        // 迭代左边
+        if(index*2 + 1 < array.length){
+            infixOrder(index*2 + 1);
+        }
+
+        // 打印当前节点
+        System.out.println(array[index]);
+
+        // 迭代右边
+        if(index*2+2 < array.length){
+            infixOrder(index*2+2);
         }
     }
+
+    // 后序遍历二叉树顺序存储结构
+    public void postOrder(int index){
+        // 迭代左边
+        if(index*2 + 1 < array.length){
+            postOrder(index*2 + 1);
+        }
+
+        // 迭代右边
+        if(index*2+2 < array.length){
+            postOrder(index*2+2);
+        }
+
+        // 打印当前节点
+        System.out.println(array[index]);
+    }
+
+
+
+
 }
