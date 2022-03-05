8bb5eb7ee963e87a6d1b2154de6c4d907487205d
StanLong <youlinbhz@sina.cn>
2022-03-05 19:07:34 +0800

    平衡二叉树

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -1,10 +1,8 @@
 package com.stanlong;
 
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.Getter;
-import lombok.ToString;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 /**
  * 平衡二叉树
@@ -12,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 public class DataStructure {
 
     public static void main(String[] args) throws Exception {
-        // int[] arr = {45, 12, 53, 3, 37, 100, 24, 61, 90, 78};
-        int[] arr = {45, 12, 53, 3, 37, 24};
+        int[] arr = {45, 12, 53, 3, 37, 100, 24, 61, 90, 78};
+        // int[] arr = {45, 12, 53, 3, 2};
         AVLTree avlTree = new AVLTree();
         for(int i=0; i<arr.length; i++){
             avlTree.add(new Node(arr[i]));
@@ -21,72 +19,22 @@ public class DataStructure {
         System.out.println("------中序遍历------");
         avlTree.infixOrder();
 
-        // 树的高度
-        System.out.println("树的高度: " + avlTree.getRoot().height());
+        System.out.println("树的高度: " + avlTree.root.height());
+        System.out.println("左子树的高度: " + avlTree.root.leftHeight());
+        System.out.println("右子树的高度: " + avlTree.root.rightHeight());
 
-        // 左子树的高度
-        System.out.println("左子树的高度: " + avlTree.getRoot().leftHeight());
-
-        // 右子树的高度
-        System.out.println("右子树的高度: " + avlTree.getRoot().rightHeight());
     }
 }
 
 // Node 节点
-@Setter
-@Getter
 @ToString(of={"value"})
 @RequiredArgsConstructor
-class Node{
+class Node<E>{
     @NonNull
-    private int value;
-    private Node left;
-    private Node right;
-
-    /**
-     * 用递归的方式添加节点
-     * @param node 要添加的节点
-     */
-    public void add(Node node){
-        if(node == null){
-            return;
-        }
-        // 左子节点都比根节点小
-        if(node.value < this.value){
-            if(this.left == null){
-                this.left = node;
-            }else {
-                this.left.add(node); // 递归往左子树添加
-            }
-        }
-        // 右子节点都比根节点大, 如果值相同，将该子节点放在左右均可
-        if(node.value >= this.value){
-            if(this.right == null){
-                this.right = node;
-            }else {
-                this.right.add(node);
-            }
-        }
-
-        // 加入新的节点后，判断树是否平衡
-        if(leftHeight() - rightHeight() >= 2){ // 左比右高， 进行右旋转
-            rightRotate();
-        }
-        if(leftHeight() - rightHeight() <= -2) { // 右比左高，进行左旋转
-
-        }
-    }
-
-    // 中序遍历
-    public void infixOrder(){
-        if(this.left != null){
-            this.left.infixOrder();
-        }
-        System.out.println(this);
-        if(this.right != null){
-            this.right.infixOrder();
-        }
-    }
+    int value;
+    Node parent; // 父节点，辅助树AVL树的旋转和节点的删除
+    Node left;
+    Node right;
 
     // 查找节点
     public Node search(int value){
@@ -99,21 +47,6 @@ class Node{
         }else{ // 往右子树递归
             if(this.right != null){
                 return this.right.search(value);
-            }
-        }
-        return null;
-    }
-
-    // 查找父节点
-    public Node searchParent(int value){
-        if((this.left != null && this.left.value == value) ||
-                (this.right != null && this.right.value == value)){ // 当前节点就是要删除节点的父节点
-            return this;
-        }else { //
-            if(value < this.value && this.left != null){ // 向左子树递归
-                return this.left.searchParent(value);
-            }else if(value >= this.value && this.right != null){ // 向右子树递归
-                return this.right.searchParent(value);
             }
         }
         return null;
@@ -148,60 +81,188 @@ class Node{
         }
         return right.height();
     }
-
-    /**
-     * 右旋转
-     */
-    public void rightRotate(){
-        // 1. 创建一个新的节点，值等于当前节点的值
-        Node newRight = new Node(value);
-
-        // 2. 把新节点的右子树，设置为当前节点的右子树
-        newRight.right = right;
-
-        // 3. 把新节点的左子树设置为当前节点左子树的右子树
-        newRight.left = left.right;
-
-        // 4. 把当前节点的值换为左子节点的值
-        value = left.value;
-
-        // 5. 把当前节点的左子树设置为左子树的左子树
-        left = left.left;
-
-        // 6. 把当且节点的右子树设置为新节点
-        right = newRight;
-
-    }
-
-    /**
-     * 左旋转
-     */
-    public void leftRotate(){
-
-    }
-
 }
 
 /**
  * 平衡二叉树
  */
 class AVLTree{
-    @Getter
-    private Node root;
+    Node root;
+    private final int LEFT = 1;
+    private final int RIGHT = -1;
+    private final int MAX_LEFT = 2;
+    private final int MAX_RIGHT = -2;
 
     public void add(Node node){
         if(root == null){
             root = node;
-        }else {
-            root.add(node);
+            return;
+        }
+        int t; // 与根节点的差值
+        Node tempBefore; // 遍历的节点的前一个节点，相当于遍历节点的父节点
+        Node temp = root;
+        do {
+            tempBefore = temp;
+            t = node.value - temp.value;
+            if(t < 0) { // 比根节点小
+                temp = temp.left; // 向左递归
+            }
+            else if(t > 0) { // 比根节点大
+                temp = temp.right; // 向右递归
+            }
+            else {
+                // 说明是已经包含相同值，不允许插入
+                return;
+            }
+        } while(temp != null);
+        node.parent = tempBefore; // 记录节点的父节点，方便旋转和删除操作
+        if(t < 0) {
+            tempBefore.left = node;
+        }
+        else {
+            tempBefore.right = node;
+        }
+        rebuild(tempBefore);
+    }
+
+    /**
+     * 平衡二叉树的方法
+     * @param node 最小不平衡树的根节点
+     */
+    public void rebuild(Node node) {
+        // 向parent循环判断是否需要需要旋转
+        while(node != null) {
+            if(calcNodeBalanceValue(node) == MAX_LEFT) {
+                fixAfterInsertion(node, LEFT);
+            }
+            else if(calcNodeBalanceValue(node) == MAX_RIGHT) {
+                fixAfterInsertion(node, RIGHT);
+            }
+            node = node.parent;
         }
     }
 
+    /**
+     * 计算node节点的BF值
+     * @param node 要计算BF的节点
+     * @return node的BF值
+     */
+    public int calcNodeBalanceValue(Node node) {
+        if(node != null) {
+            return node.leftHeight() - node.rightHeight();
+        }
+        return 0;
+    }
+
+    /**
+     * 调整树的结构
+     * @param node 最小不平衡树的根节点
+     * @param type 树的不平衡类型
+     */
+    public void fixAfterInsertion(Node node, int type) {
+        if(type == LEFT) {
+            Node left = node.left;
+            // 右旋
+            if(left.left != null) {
+                rightRotation(node);
+            }
+            // 左右旋
+            else if(left.right != null) {
+                leftRotation(left);
+                rightRotation(node);
+            }
+        }
+        else if(type == RIGHT) {
+            Node right = node.right;
+            // 左旋
+            if(right.right != null) {
+                leftRotation(node);
+            }
+            // 右左旋
+            else if(right.left != null) {
+                rightRotation(right);
+                leftRotation(node);
+            }
+        }
+    }
+
+    /**
+     * 右旋
+     * 这段比较难理解，注释按照文档【图2 一般形式的LL型调整】补充
+     * @param node BF的绝对值大于1的节点
+     * @return 旋转后的结果
+     */
+    public Node rightRotation(Node node) {
+        if (node != null) { // 节点A不为null
+            Node left = node.left; // left为A的左节点B
+            node.left = left.right; // A的左节点重新指向A的左节点的右节点， 就像图里的A的左节点由B变成了B的右节点BR
+            // 如果B的右子节点存在，则调整B的右子节点的父节点为A
+            if (left.right != null) {
+                left.right.parent = node;
+            }
+            left.parent = node.parent; // 调整B的位置
+            if (node.parent == null) { // 如果A为原树的根节点
+                this.root = left; // 调整后B为原树的新的根节点
+            }
+            else if (node.parent.right == node) {// 如果A是原树是右子树的节点， 即原树右子树失衡
+                node.parent.right = left; // 原树右子树指向调整后的B节点
+            }
+            else if (node.parent.left == node) { // 如果A是原树是左子树的节点， 即原树左子树失衡
+                node.parent.left = left; //  原树左子树指向调整后的B节点
+            }
+            left.right = node; // B的右节点指向A
+            node.parent = left; // A的父节点指向B
+            return left; // 返回调整后的节点B
+        }
+        return null;
+    }
+
+    /**
+     * 左旋
+     * 这段比较难理解，可参考右旋的代码注释和【图4 一般形式的RR型调整】来理解
+     * @param node BF绝对值大于2的节点
+     * @return 调整后的节点
+     */
+    public Node leftRotation(Node node) {
+        if (node != null) {
+            Node right = node.right;
+            node.right = right.left;
+            if (right.left != null) {
+                right.left.parent = node;
+            }
+            right.parent = node.parent;
+            if(node.parent == null) {
+                this.root = right;
+            }
+            else if(node.parent.right == node) {
+                node.parent.right = right;
+            }
+            else if(node.parent.left == node) {
+                node.parent.left = right;
+            }
+            right.left = node;
+            node.parent = right;
+            return right;
+        }
+        return null;
+    }
+
+    /**
+     * 中序遍历
+     */
     public void infixOrder(){
-        if(root != null){
-            root.infixOrder();
-        }else {
-            System.out.println("树为空！");
+        this.infixOrder(this.root);
+    }
+
+    /**
+     * 中序遍历
+     * @param node 从根节点开始遍历
+     */
+    public void infixOrder(Node node){
+        if(node != null){
+            this.infixOrder(node.left);
+            System.out.println(node);
+            this.infixOrder(node.right);
         }
     }
 
@@ -210,14 +271,6 @@ class AVLTree{
             return null;
         }else {
             return root.search(value);
-        }
-    }
-
-    public Node searchParent(int value){
-        if(root == null){
-            return null;
-        }else {
-            return root.searchParent(value);
         }
     }
 
@@ -230,37 +283,37 @@ class AVLTree{
                 return;
             }
             // 如果该二叉树只有一个节点，那删除之后就是一个空树了
-            if(root.getLeft() == null && root.getRight() == null && root.getValue() == value){
+            if(root.left == null && root.right == null && root.value == value){
                 root = null;
                 return;
             }
 
             // 查找该节点的父节点
-            Node parent = searchParent(value);
+            Node parent = target.parent;
 
             // 1. 如果要删除的节点是叶子节点, 则直接删除该节点
-            if(target.getLeft() == null && target.getRight() == null){
-                if(parent.getLeft() != null && parent.getLeft().getValue() == value){ // 要删除的节点是左子节点
-                    parent.setLeft(null);
-                }else if(parent.getRight() != null && parent.getRight().getValue() == value) { // 要删除的是右子节点
-                    parent.setRight(null);
+            if(target.left == null && target.right == null){
+                if(parent.left != null && parent.left.value == value){ // 要删除的节点是左子节点
+                    parent.left = null;
+                }else if(parent.right != null && parent.right.value == value) { // 要删除的是右子节点
+                    parent.right = null;
                 }
-            } else if(target.getLeft() != null && target.getRight() != null){// 3.被删除的节点既有左子树，也有右子树
-                int minValue = delRightMin(target.getRight()); // 删除并返回右子树节点的最小值
-                target.setValue(minValue);  // 用右子树的最小值替换要删除的节点
+            } else if(target.left != null && target.right != null){// 3.被删除的节点既有左子树，也有右子树
+                int minValue = delRightMin(target.right); // 删除并返回右子树节点的最小值
+                target.value = minValue;  // 用右子树的最小值替换要删除的节点
             }else { // 2. 被删除的节点只有左子树或者只有右子树，用其左子树或者右子树替换该节点
                 // 要删除的节点只有左子树
-                if(target.getLeft() != null){
-                    if(parent.getLeft() != null && parent.getLeft().getValue() == value){ //要删除的节点是父节点左子节点
-                        parent.setLeft(target.getLeft());
+                if(target.left != null){
+                    if(parent.left != null && parent.left.value == value){ //要删除的节点是父节点左子节点
+                        parent.left = target.left;
                     }else { // 要删除的节点是父节点的右子节点
-                        parent.setRight(target.getLeft());
+                        parent.right = target.left;
                     }
                 }else { // 要删除的节点有右子树
-                    if(parent.getRight() != null && parent.getRight().getValue() == value){ //要删除的节点是父节点左子节点
-                        parent.setRight(target.getRight());
+                    if(parent.right != null && parent.right.value == value){ //要删除的节点是父节点左子节点
+                        parent.right = target.right;
                     }else { // 要删除的节点是父节点的右子节点
-                        parent.setLeft(target.getRight());
+                        parent.left = target.right;
                     }
 
                 }
@@ -275,10 +328,10 @@ class AVLTree{
      */
     public int delRightMin(Node node){
         Node target = node;
-        while(target.getLeft() != null){
-            target = target.getLeft();
+        while(target.left != null){
+            target = target.left;
         }
-        delNode(target.getValue());
-        return target.getValue();
+        delNode(target.value);
+        return target.value;
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 每一个项目都是比较散的知识
 
 数据结构与算法笔记： https://docs-lemon.view6view.club/#/src/study/2
+
+数据结构可视化网站：https://visualgo.net/zh/bst?slide=3-6
+
