c28fabd238e93fffa7734ed4bad2e95072f11c1c
StanLong <youlinbhz@sina.cn>
2022-03-07 22:45:12 +0800

    图的遍历

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 /**
  * 图的快速入门
+ * 这是一个不带权的无向图， 邻接矩阵延左上右下对称
  * 思路分析
  * 1. 使用 ArrayList 存储图的顶点
  * 2. 使用二维数组保存矩阵
+ *
  */
 public class DataStructure {
-
-
 
     public static void main(String[] args) throws Exception {
         Graph graph = new Graph(5);
@@ -33,6 +33,10 @@ public class DataStructure {
         // 遍历
         graph.showGraph();
 
+        // 深度遍历
+        System.out.println("----深度优先----");
+        graph.dfs();
+
 
     }
 }
@@ -41,12 +45,14 @@ class Graph{
     private ArrayList<String> vertexList; // 存储定点
     private int[][] edges;  // 存储图对应的邻接矩阵的边
     private int numOfEdges; // 边的数目
+    private boolean[] isVisited;  // 记录被访问过的节点
 
     // 构造器初始化
     public Graph(int n){
         edges = new int[n][n];
         vertexList = new ArrayList<String>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
     }
 
     // 插入顶点
@@ -54,7 +60,12 @@ class Graph{
         vertexList.add(vertex);
     }
 
-    // 插入边
+    /**
+     * 插入边
+     * @param v1 边的一个节点对应的下标
+     * @param v2 边的另一个节点对应的下标
+     * @param weight 权重。 1 表示有连接， 0 表示无连接
+     */
     public void insertEdges(int v1, int v2, int weight){
         edges[v1][v2] = weight;
         edges[v2][v1] = weight;
@@ -88,4 +99,63 @@ class Graph{
         }
     }
 
+    /**
+     * 获取 index 邻接节点的下标
+     * @param index index节点的下标
+     * @return index 邻接节点的下标
+     */
+    public int getFirstNeighbour(int index){
+        for(int j =0; j<vertexList.size(); j++){
+            if(edges[index][j]>0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标获取下一个邻接节点
+     * @param v1 前一个邻接节点的下标
+     * @param v2 前一个邻接节点的下标
+     * @return 下一个邻接节点的下标
+     */
+    public int getNextNeighbour(int v1, int v2){
+        for(int j = v2 + 1; j<vertexList.size(); j++){
+            if(edges[v1][j] > 0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 深度遍历
+     * @param isVisited
+     * @param i
+     */
+    public void dfs(boolean[] isVisited, int i){
+        // 访问节点
+        System.out.print(getValueByIndex(i) + "->");
+        // 节点标记为已访问
+        isVisited[i] = true;
+        // 查找i节点的第一个邻接节点w
+        int w = getFirstNeighbour(i);
+        while (w != -1){
+            if(!isVisited[w]){
+                dfs(isVisited, w);
+            }
+            // 如果 w 已经被访问过， 就需要查找邻接节点的下一个节点
+            w = getNextNeighbour(i, w);
+        }
+    }
+
+    // 重载
+    public void dfs(){
+        // 遍历所有的节点进行dfs
+        for(int i=0; i<getNumOfEdges(); i++){
+            if(!isVisited[i]){
+                dfs(isVisited, i);
+            }
+        }
+    }
 }
