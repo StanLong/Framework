643bfd88e62dd23016ce65221ce2d49f7343748d
StanLong <youlinbhz@sina.cn>
2022-03-31 21:10:58 +0800

    Z字形变换

--- 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
+++ 04DataStructure/DataStructure/src/main/java/com/stanlong/DataStructure.java
@@ -1,91 +1,159 @@
 package com.stanlong;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
- * 弗洛伊德算法
+ * 马踏棋盘算法
  */
 public class DataStructure {
 
-    private final static int N = 65535;
-
     public static void main(String[] args) {
-        char[] vertex = {'0', '1', '2', '3'};
-        int[][] weight = {
-                    /*0*1*2*3*/
-                /*0*/{0,5,N,7},
-                /*1*/{N,0,4,2},
-                /*2*/{3,3,0,2},
-                /*3*/{N,N,1,0}};
-        Graph graph = new Graph(vertex, weight);
-        showGraph(graph);
-        floyd(graph);
-        System.out.println();
-        showGraph(graph);
-    }
-
-    public static void showGraph(Graph graph) {
-        for (int i = 0; i < graph.vertex.length; i++) {
-            System.out.printf("%8s", graph.vertex[i] + "的前驱节点为：");
-            for (int j = 0; j < graph.vertex.length; j++) {
-                System.out.printf("%5s\t", graph.pre[i][j]);
-            }
-            System.out.println();
-            System.out.printf("%8s", graph.vertex[i] + "的各点权值为：");
-            for (int j = 0; j < graph.vertex.length; j++) {
-                System.out.printf("%5s\t", graph.dis[i][j]);
+        int x = 8;
+        int y = 8;
+        int row = 1;
+        int col = 1;
+        ChessBoard chessBoard = new ChessBoard(x, y);
+        int[][] array = new int[x][y];
+        long start = System.currentTimeMillis();
+        travelChessBoard(chessBoard, array, row - 1, col - 1, 1);
+        long end = System.currentTimeMillis();
+        System.out.println("花费时间" + (end - start) + "毫秒");
+        for (int[] rows : array) {
+            for (int step : rows) {
+                System.out.printf("%2s\t", step);
             }
             System.out.println();
         }
     }
 
     /**
-     * 弗洛伊德算法
-     * @param graph
+     * 根据当前的位置，计算马儿还能走哪些位置，并放入到一个集合中（List），最多有8个位置
+     * @param chessBoard
+     * @param chessPoint
+     * @return
      */
-    public static void floyd(Graph graph) {
-        int length = graph.vertex.length;
-        int[][] dis = graph.dis;
-        int[][] pre = graph.pre;
-        // 变量保存距离
-        int len = 0;
-        for (int i = 0; i < length; i++) {
-            // 从j顶点出发
-            for (int j = 0; j < length; j++) {
-                for (int k = 0; k < length; k++) {
-                    // 从i顶点出发，经过中间顶点k，到达j
-                    len = dis[j][i] + dis[i][k];
-                    if (len < dis[j][k]) {
-                        // 更新距离
-                        dis[j][k] = len;
-                        // 更新前驱
-                        pre[j][k] = pre[i][k];
-                    }
-                }
+    public static List<Point> getAround(ChessBoard chessBoard, Point chessPoint) {
+        int X = chessBoard.x;
+        int Y = chessBoard.y;
+        int left = chessPoint.x - 2;
+        int right = chessPoint.x + 2;
+        int top = chessPoint.y + 2;
+        int bottom = chessPoint.y - 2;
+        List<Point> result = new ArrayList<>();
+        if (left > 0) {
+            if (top - 1 <= X) {
+                result.add(new Point(left, top - 1));
+            }
+            if (bottom + 1 > 0) {
+                result.add(new Point(left, bottom + 1));
             }
         }
+        if (left + 1 > 0) {
+            if (top <= X) {
+                result.add(new Point(left + 1, top));
+            }
+            if (bottom > 0) {
+                result.add(new Point(left + 1, bottom));
+            }
+        }
+        if (right - 1 <= Y) {
+            if (top <= X) {
+                result.add(new Point(right - 1, top));
+            }
+            if (bottom > 0) {
+                result.add(new Point(right - 1, bottom));
+            }
+        }
+        if (right <= Y) {
+            if (top - 1 <= X) {
+                result.add(new Point(right, top - 1));
+            }
+            if (bottom + 1 > 0) {
+                result.add(new Point(right, bottom + 1));
+            }
+        }
+        return result;
     }
+
+    /**
+     * 算法核心，不断回溯
+     * 需要注意：Point和数组下标的关系是，比如8x8的棋盘，Point就是[1,8]，数组就是[0,7]
+     * @param chessBoard 棋盘对象
+     * @param array 棋盘数组
+     * @param row 按照数组索引的行索引
+     * @param col 按照数组索引的列索引
+     * @param step 步数
+     */
+    public static void travelChessBoard(ChessBoard chessBoard, int[][] array, int row, int col, int step) {
+        // 标记位置已经转换
+        array[row][col] = step;
+        chessBoard.visited[row][col] = true;
+        List<Point> around = getAround(chessBoard, new Point(col + 1, row + 1));
+        // 对around进行排序
+        sort(chessBoard, around);
+        while (!around.isEmpty()) {
+            // 取出一个可以走的位置
+            Point remove = around.remove(0);
+            int tempCol = remove.x - 1;
+            int tempRow = remove.y - 1;
+            // 判断该点是否已经访问过
+            if (!chessBoard.visited[tempRow][tempCol]) {
+                // 说明没有访问过
+                travelChessBoard(chessBoard, array, tempRow, tempCol, step + 1);
+            }
+        }
+
+        if (step < chessBoard.x * chessBoard.y && !chessBoard.finish) {
+            array[row][col] = 0;
+            chessBoard.visited[row][col] = false;
+        } else {
+            chessBoard.finish = true;
+        }
+    }
+
+    /**
+     * 贪心的思想：根据当前这个所有的下一步选择位置，进行递减排序，减少回溯可能
+     * @param chessBoard
+     * @param pointList
+     */
+    public static void sort(ChessBoard chessBoard, List<Point> pointList) {
+        pointList.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                // 获取到o1的下一步的所有位置个数
+                int count1 = getAround(chessBoard, o1).size();
+                // 获取到o2的下一步的所有位置个数
+                int count2 = getAround(chessBoard, o2).size();
+                return count1 - count2;
+            }
+        });
+    }
+
 
 
 }
 
-class Graph {
-    // 存放顶点的数组
-    char[] vertex;
+class ChessBoard {
 
-    // 保存从各个顶点出发到其他顶点的距离
-    int[][] dis;
+    // 棋盘的行数
+    int y;
 
-    // 保存到目标顶点的前驱节点
-    int[][] pre;
+    // 棋盘的列数
+    int x;
 
-    public Graph(char[] vertex, int[][] matrix) {
-        this.vertex = vertex.clone();
-        this.dis = matrix.clone();
-        this.pre = new int[vertex.length][vertex.length];
-        // 对pre数组初始化
-        for (int i = 0; i < vertex.length; i++) {
-            Arrays.fill(pre[i], i);
-        }
+    // 标记每个数组是否被访问过
+    boolean[][] visited;
+
+    // 标记是否所有点都被访问过了
+    boolean finish;
+
+    public ChessBoard(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.visited = new boolean[x][y];
+        this.finish = false;
     }
 }
