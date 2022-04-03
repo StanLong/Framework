8cf8c854b540a8655faa8fb99b6b6ceccea39b85
StanLong <youlinbhz@sina.cn>
2022-04-03 21:00:34 +0800

    建造者模式

--- 06DesignPattern/DesignPattern/src/main/java/com/stanlong/DesignPattern.java
+++ 06DesignPattern/DesignPattern/src/main/java/com/stanlong/DesignPattern.java
@@ -1,56 +1,73 @@
 package com.stanlong;
 
-import lombok.*;
+import lombok.Setter;
 
 public class DesignPattern {
     public static void main(String[] args) {
-        System.out.println("----原型模式，浅拷贝-----");
-
-        Sheep sheep = new Sheep("喜羊羊", 10);
-        sheep.setFriend(new Sheep("美羊羊", 8));
-        Sheep sheep1 = (Sheep) sheep.clone();
-        Sheep sheep2 = (Sheep) sheep.clone();
-
-        System.out.println("sheep = " + sheep + ": " + sheep.getFriend().hashCode());
-        System.out.println("sheep1 = " + sheep1 + ": " + sheep1.getFriend().hashCode());
-        System.out.println("sheep2 = " + sheep2 + ": " + sheep2.getFriend().hashCode());
-
-        System.out.println("sheep == sheep1?: " + (sheep == sheep1));
-        System.out.println("sheep == sheep2?: " + (sheep == sheep2));
-        System.out.println("sheep1 == sheep2?: " + (sheep1 == sheep2));
-
-        System.out.println(sheep.hashCode());
-        System.out.println(sheep1.hashCode());
-        System.out.println(sheep2.hashCode());
+        Builder builder = new ConcreteBuilder();
+        Director director = new Director(builder);
+        Product product = director.construct();
+        product.show();
     }
 }
 
-@Getter
+/**
+ * 1. 产品角色：包含多个组成部件的复杂对象。
+ */
 @Setter
-@ToString(exclude = "friend")
-@RequiredArgsConstructor
-class Sheep implements Cloneable { // 原型类需要声明一个克隆自己的接口
-    @NonNull
-    private String name;
-    @NonNull
-    private int age;
+class Product {
+    private String partA;
+    private String partB;
+    private String partC;
 
-    private Sheep friend; // 对象
-
-
-    //克隆该实例，使用默认的clone方法来完成
-    @Override
-    protected Object clone()  {
-
-        Sheep sheep = null;
-        try {
-            sheep = (Sheep)super.clone();
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(e.getMessage());
-        }
-        // TODO Auto-generated method stub
-        return sheep;
+    public void show() {
+        System.out.println("产品由A，B，C三部分组成");
     }
+}
 
+/**
+ * 2. 抽象建造者：包含创建产品各个子部件的抽象方法。
+ */
+abstract class Builder {
+    //创建产品对象
+    protected Product product = new Product();
+    public abstract void buildPartA();
+    public abstract void buildPartB();
+    public abstract void buildPartC();
+    //返回产品对象
+    public Product getResult() {
+        return product;
+    }
+}
+
+/**
+ * 3.  具体建造者：实现了抽象建造者接口。
+ */
+class ConcreteBuilder extends Builder {
+    public void buildPartA() {
+        product.setPartA("建造 PartA");
+    }
+    public void buildPartB() {
+        product.setPartB("建造 PartB");
+    }
+    public void buildPartC() {
+        product.setPartC("建造 PartC");
+    }
+}
+
+/**
+ *  指挥者：调用建造者中的方法完成复杂对象的创建
+ */
+class Director {
+    private Builder builder;
+    public Director(Builder builder) {
+        this.builder = builder;
+    }
+    //产品构建与组装方法
+    public Product construct() {
+        builder.buildPartA();
+        builder.buildPartB();
+        builder.buildPartC();
+        return builder.getResult();
+    }
 }
