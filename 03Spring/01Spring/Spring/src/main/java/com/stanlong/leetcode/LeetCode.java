037752e4e28c6498c776b3c018b38cb53a05d7b1
StanLong <youlinbhz@sina.cn>
2022-04-17 12:01:23 +0800

    准备考OD

--- 03Spring/01Spring/Spring/src/main/java/com/stanlong/leetcode/LeetCode.java
+++ 03Spring/01Spring/Spring/src/main/java/com/stanlong/leetcode/LeetCode.java
@@ -1,26 +1,36 @@
 package com.stanlong.leetcode;
 
+import java.util.Stack;
+
 public class LeetCode {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "aa";
-        String p = "a*";
-        System.out.println(solution.isMatch(s, p));
+        String s = "deeedbbcccbdaa";
+        int k = 3;
+        System.out.println(solution.removeDuplicates(s, k));
 
     }
 }
 
 class Solution {
-    public boolean isMatch(String s, String p){
-        if (p.isEmpty()){
-            return s.isEmpty();
+    public String removeDuplicates(String s, int k){
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder(s);
+        while (sb.length() != 0){
+            for(int i=0; i<sb.length();++i){
+                if(i==0 || sb.charAt(i) != sb.charAt(i-1)){
+                    stack.push(1);
+                }else {
+                    int incremented = stack.pop() + 1;
+                    if(incremented==k){
+                        sb.delete(i-k+1, i+1);
+                        i = i-k;
+                    }else {
+                        stack.push(incremented);
+                    }
+                }
+            }
         }
-        boolean firstMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-
-        if (p.length() > 1 && p.charAt(1) == '*'){
-            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
-        } else{
-            return firstMatch && isMatch(s.substring(1), p.substring(1));
-        }
+        return sb.toString();
     }
 }
