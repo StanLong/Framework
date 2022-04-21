75b642ccb60a8a6dc419a05473b52f4b5b876367
StanLong <youlinbhz@sina.cn>
2022-04-21 12:08:10 +0800

    JdbcTemplate

---
+++ 03Spring/01Spring/Spring/src/main/java/com/stanlong/bean/Tb_User.java
@@ -0,0 +1,15 @@
+package com.stanlong.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Tb_User {
+
+	private Integer id;
+	private String username;
+	private String password;
+}
