bd233411b23fe24a781a117255d52fb26e0f180b
StanLong <youlinbhz@sina.cn>
2022-05-11 21:55:05 +0800

    OD考试

--- 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
+++ 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
@@ -1,37 +1,15 @@
 package com.stanlong.leetcode;
 
+import java.util.*;
+
 public class LeetCode {
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
-        int k = 2;
-        System.out.println(s.findMaxConsecutiveOnes(nums, k));
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        String[] strs = scanner.nextLine().split(",");
+        Map<Character, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
 
-class Solution {
-    public int findMaxConsecutiveOnes(int[] nums, int k) {
-        int len = nums.length;
-        int left = 0;
-        int right = 0;
-        int max = 0;
-        int counter = 0;
 
-        for (int num : nums) {
-            while (right < len && counter < k) {
-                max = Math.max(max, left - right);
-                if (num == 0) {
-                    counter++;
-                }
-                right++;
-            }
-            while (left < right && counter >= k) {
-                if (nums[left] == 0) {
-                    counter--;
-                }
-                left++;
-            }
-        }
-        return max;
+
     }
 }
