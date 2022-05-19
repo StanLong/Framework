d26afe93fb43727748ede016932492d79ea175eb
StanLong <youlinbhz@sina.cn>
2022-05-19 22:23:15 +0800

    OD考试

--- 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
+++ 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
@@ -1,12 +1,30 @@
 package com.stanlong.leetcode;
 
+import java.util.*;
+
 public class LeetCode {
 
     public static void main(String[] args) {
-        String binaryA = Integer.toBinaryString(3);
-        System.out.println(binaryA);
-        System.out.println(Integer.valueOf(binaryA));
-        String hexA = String.format("%08d", Integer.valueOf(binaryA));
-        System.out.println(hexA);
+        Scanner scanner = new Scanner(System.in);
+        int m = Integer.parseInt(scanner.nextLine());
+        String[] str = scanner.nextLine().split(" ");
+        int[] nums = new int[m];
+        for(int i=0; i< str.length; i++){
+            nums[i] = Integer.parseInt(str[i]);
+        }
+        int[] result = new int[m];
+        for(int i=0; i<m; i++){
+            for(int j=i+1; j<m; j++){
+                if(nums[j] > nums[i]){
+                    result[i]=j;
+                    break;
+                }
+                if(j==m-1){
+                    result[i] = 0;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(result));
+
     }
 }
