faec02ce01e6e3a6a579c41f6f26caa132afb8bb
StanLong <youlinbhz@sina.cn>
2022-05-22 14:20:54 +0800

    OD考试

--- 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
+++ 03Spring/03SpringBoot/SpringBoot/src/main/java/com/stanlong/leetcode/LeetCode.java
@@ -1,30 +1,35 @@
 package com.stanlong.leetcode;
 
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.util.Scanner;
 
 public class LeetCode {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = Integer.parseInt(scanner.nextLine());
-        String[] str = scanner.nextLine().split(" ");
-        int[] nums = new int[m];
-        for(int i=0; i< str.length; i++){
-            nums[i] = Integer.parseInt(str[i]);
+        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        int target = scanner.nextInt();
+        int[] nums = new int[target];
+        for(int i=0; i< nums.length; i++){
+            nums[i] = scanner.nextInt();
         }
-        int[] result = new int[m];
-        for(int i=0; i<m; i++){
-            for(int j=i+1; j<m; j++){
-                if(nums[j] > nums[i]){
-                    result[i]=j;
+        int A = 0;
+        int B = 0;
+        int C = 0;
+        boolean flag = false;
+        for(int i=0; i< nums.length; i++){
+            A = nums[i];
+            for(int j=nums.length-i-1; j>= 0; j--){
+                B = nums[j];
+                C = nums[nums.length-i-j-1];
+                if(A == B + 2 * C &&(A!=B) &&(A!=C) && (B!=C)){
+                    flag = true;
+                    System.out.println(A + " " + B + " " + C);
                     break;
-                }
-                if(j==m-1){
-                    result[i] = 0;
                 }
             }
         }
-        System.out.println(Arrays.toString(result));
-
+        if(!flag){
+            System.out.println(0);
+        }
     }
 }
